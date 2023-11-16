aa6bdca74a9648dc65ba58c1d81f821773c19afb
Deepak-ic <deepakdevil428@gmail.com>
2023-11-16 16:01:14 +0530

    update

---
+++ src/org/expn/Challenge4.java
@@ -0,0 +1,23 @@
+package org.expn;
+
+public class Challenge4 {
+	public static void main(String[] args){
+		try{
+		System.out.printf("1");
+		int data = 5 / 0;
+		}
+		catch(ArithmeticException e){
+		Throwable obj = new Throwable("Sample");
+		try{
+		throw obj;
+		}
+		catch (Throwable e1) {
+		System.out.printf("8");
+		}
+		}
+		finally{
+		System.out.printf("3");
+		}
+		System.out.printf("4");
+		}
+}
